15a448d6c03773e1788d3478c5a9b29cf68a6866
unknown <adi.shrivastava2905@gmail.com>
2022-05-28 02:50:54 +0530

    second last commit second time

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './scss/App.css';
 import Homepage from './components/Homepage';
 import Download from './components/Download';
 import CLI from './components/CLI';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 
 function App() {
 	return (
