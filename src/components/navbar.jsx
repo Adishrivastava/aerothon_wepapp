50fdc6abc34c4bee1d05b9365682f72eec3abcd1
unknown <adi.shrivastava2905@gmail.com>
2022-05-28 02:31:05 +0530

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import image from '../assets/icons8-naira-96.png';
 
 const Navbar = (props) => {
 	// const navigate = useNavigate();
 	return (
-		<div className="navbar" onClick={console.log('hello')}>
+		<div className="navbar">
 			<a href="/">
-				<div className="title">
-					<img src={image} alt="N" />
-					{/* NHP */}
-				</div>
+				<div className="title">NHP</div>
 			</a>
 		</div>
 	);
